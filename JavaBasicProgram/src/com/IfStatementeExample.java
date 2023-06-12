5a133e506f6f91915526586affcd3b06c35b4f8a
Andre Bessa <andrefsbessa@outlook.com>
2023-06-12 10:43:21 +0000

    ifstatement added

---
+++ JavaBasicProgram/src/com/IfStatementeExample.java
@@ -0,0 +1,23 @@
+package com;
+
+public class IfStatementeExample {
+
+	public static void main(String[] args) {
+//		int age = 21;
+//		if(age>=21) {
+//			System.out.println("You can vote!");
+//		}
+//		else {
+//			System.out.println("Underage");
+//		}
+//		System.out.println("Finish");
+		int a=10, b=50;
+		if (a>b) {
+			System.out.println("A is largest");
+		}else {
+			System.out.println("B is largest");
+		}
+		System.out.println("Finish");
+	}
+
+}
