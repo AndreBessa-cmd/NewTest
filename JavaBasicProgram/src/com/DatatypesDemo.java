504657c7428300d5a895d3c28e13b5f7339d3afb
Andre Bessa <andrefsbessa@outlook.com>
2023-06-12 09:51:50 +0000

    operators added

---
+++ JavaBasicProgram/src/com/DatatypesDemo.java
@@ -0,0 +1,22 @@
+package com;
+
+public class DatatypesDemo {
+
+	public static void main(String[] args) {
+		int a;
+		int b=10;
+		a= b+10;
+		boolean result = true;
+		char abc ='$';
+		int sum =a +b;
+		System.out.println(b);
+		System.out.println("Value of A is "+a);
+		System.out.println(a+b);
+		System.out.println(abc);
+		System.out.println(result);
+		System.out.println("Sum of two number is "+(a+b));
+		System.out.println("Sum of two number is "+sum);
+
+	}
+
+}
